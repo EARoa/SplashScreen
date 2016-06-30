5edbb13ba6f9f934d1ee89c7851b01391908e364
Efrain <jesusayllonroa@gmail.com>
2016-06-30 09:05:06 -0500

    IT WORKS

--- SplashScreen/ViewController.m
+++ SplashScreen/ViewController.m
@@ -16,45 +16,68 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    splashScreen = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    splashScreen.backgroundColor = [UIColor colorWithRed:45/255.0 green:62/255.0 blue:79/255.0 alpha:1.0];
+    
+    
+    splashText = [[UILabel alloc]initWithFrame:CGRectMake(145,313, 125, 40)];
+    [splashText setText:@"Label"];
+    [splashText setTextColor:[UIColor whiteColor]];
+    [splashScreen addSubview:splashText];
 
-    [self firstTimeView];
+    [self fadeUIViewOut];
     
     
 }
 
--(void) firstTimeView {
+
+
+
+
+-(void) fadeUIViewOut {
+
     
     
     if (![@"1" isEqualToString:[[NSUserDefaults standardUserDefaults]
                                 objectForKey:@"aValue"]]) {
         [[NSUserDefaults standardUserDefaults] setValue:@"1" forKey:@"aValue"];
         [[NSUserDefaults standardUserDefaults] synchronize];{
+           
         
-        UIView *splashScreen = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-        splashScreen.backgroundColor = [UIColor greenColor];
-        
-        splashScreen.alpha = 1.0f;
-        //  Fades out after X seconds
-        [UIView animateWithDuration:0.5 delay:2.0 options:0 animations:^{
-            // Changes Alpha value to 0
-            splashScreen.alpha = 0.0f;
-        } completion:^(BOOL finished) {
-            // Once the animation is completed and view is transparent, view hides
-            splashScreen.hidden = YES;
-        }];
-        
-        [self.view addSubview:splashScreen];
-        NSLog(@"Hello!");
-        
+            splashScreen.alpha = 1.0f;
+            //  Fades out after X seconds
+            [UIView animateWithDuration:.5 delay:4.0 options:0 animations:^{
+                // Changes Alpha value to 0
+                splashScreen.alpha = 0.0f;
+            } completion:^(BOOL finished) {
+                // Once the animation is completed and view is transparent, view hides
+                splashScreen.hidden = YES;
+            }];
+            
+            
+            splashText.alpha = 0.0f;
+            [UILabel animateWithDuration:.5 delay:1.0 options:0 animations:^{
+                // Changes Alpha value to 0
+                splashText.alpha = 1.0f;
+            } completion:^(BOOL finished) {
+                splashText.alpha = 1.0f;
+                [UILabel animateWithDuration:.5 delay:2.0 options:0 animations:^{
+                    // Changes Alpha value to 0
+                    splashText.alpha = 0.0f;
+                } completion:^(BOOL finished) {
+                    // Once the animation is completed and view is transparent, text hides
+                    splashText.hidden = YES;
+                }];
+            }];
+            [self.view addSubview:splashScreen];
+            NSLog(@"Fading In!");
         }
     }
 
-
-
-
-
-}
-
+    
+    
+   }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
